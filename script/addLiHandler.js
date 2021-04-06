99923b0114bd68c10ef3fd94604692522e2914c8
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-06 16:22:04 +0200

    add freecodecamp script and minor refactor

--- script/addLiHandler.js
+++ script/addLiHandler.js
@@ -1,19 +1,11 @@
 (function () {
-  const liItems = document.getElementsByClassName('nav-item');
   const navHeaderMenu = document.getElementById('docs-sidebar');
 
   window.onload = function () {
     'use strict';
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        // e.preventDefault();
-
-        // document.querySelector(this.getAttribute('href')).scrollIntoView({
-        //   behavior: 'smooth',
-        //   block: 'center',
-        // });
-
+      anchor.addEventListener('click', function () {
         navHeaderMenu.classList.toggle('toggleMenu');
       });
     });
